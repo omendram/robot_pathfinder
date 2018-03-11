3b09dd18ad6f0b93383be872d3d9560a44a95dc7
KiraRuschmeier <37103232+KiraRuschmeier@users.noreply.github.com>
2018-03-11 15:14:56 +0100

    Add files via upload

---
+++ ObjectMoving/ObjectMoving/ObjectMoving.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace ObjectMoving
+{
+    partial class ObjectMoving
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Timer tmrMoving;
+            tmrMoving = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // tmrMoving
+            // 
+            tmrMoving.Enabled = true;
+            tmrMoving.Tick += new System.EventHandler(this.TmrMoving_Tick);
+            // 
+            // FormView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Name = "FormView";
+            this.Text = "FormView";
+            this.Load += new System.EventHandler(this.TmrMoving_Tick);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FormView_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
