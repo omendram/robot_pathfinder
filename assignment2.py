c5cb41f8e3efb81f4215e5ab72aba45947ef960a
JacobSalam <jacob_salam@hotmail.com>
2018-03-19 16:12:34 +0100

    Added Sample Odometry Motion Model and believe motion

--- assignment2.py
+++ assignment2.py
@@ -2,7 +2,7 @@
 import pygame
 import random, sys, time, math, numpy, shapely
 from numpy import linalg
-from random import randint
+from random import randint, uniform
 from shapely.geometry import LineString
 from random import *
  
@@ -23,24 +23,36 @@ pygame.display.set_caption("Maze")
  
 clock = pygame.time.Clock()
 
-angle = 50
+angle = 0
 speed = 2
 
 
-x = 74
-y = 100
+x = 90
+y = 90
+believeX = 90
+believeY = 90
+xBelieveMotion = speed* math.cos(math.radians(angle))
+yBelieveMotion = speed* math.sin(math.radians(angle))
+
 xMotion =speed* math.cos(math.radians(angle))
-yMotion =speed* math.cos(math.radians(angle));
+yMotion =speed* math.sin(math.radians(angle))
 radius = 50
 rotx=0
 roty=0
-xpos=75
-ypos=60
+xpos=0
+ypos=0
+rot1=0
+rot2=0
+trans =0
 directionAngle =0
+noise1 = 0.8 # noise for rot1 and 2
+noise2 = 0.8 # trans noise for rot1 and 2
+noise3 = 0.8 # trans noise for trans
+noise4 = 0.1 # rot noise for trans
 
 #    or (y0 >= y1-50 and y0 <= y2+50) wallList = [[10,10,1070,10],[10,10,10,710],[10,710,1070,710],[1070,10,1070,710],[265,10,265,238],[269,10,269,234],[269,234,536,234],
  #          [265,238,532,238],[532,238,532,470],[536,238,536,474],[532,470,265,470],[536,474,265,474],[799,710,799,236],[803,710,803,236],[799,236,803,236]]
-wallList = [[10,10,1070,10],[10,10,10,710],[10,710,1070,710],[1070,10,1070,710],[267,10,267,236],[534,236,534,472],[267,472,534,472],[801,236,801,710]]
+wallList = [[10,10,1070,10],[10,10,10,710],[10,710,1070,710],[1070,10,1070,710],[267,10,267,236],[534,236,534,472],[10,472,534,472],[801,10,801,472]]
 
 
 
@@ -50,8 +62,8 @@ def updateAngle(x,y,nextx,nexty):
     return math.atan2(nexty-y,nextx-x)
 
 def calcDirection(x,y):
-    rotx=x + (radius-1) * math.cos(angle)
-    roty=y + (radius-1)* math.sin(angle)
+    rotx=x + (radius-1) * math.cos(math.radians(angle))
+    roty=y + (radius-1)* math.sin(math.radians(angle))
     return rotx, roty
 
 def makeWalls(wall):
@@ -70,18 +82,21 @@ def CheckCollision(wall,xpos1,ypos1):
         return False
 
 #updates the position of the robot based on its motion
-def updatePosition(x,y,xMotion,yMotion,angle):
+def updatePosition(x,y,xMotion,yMotion):
     x = x + xMotion
     y = y + yMotion
     xpos = int(x)
     ypos = int(y)
 
-    sensors(xpos,ypos,angle)
-
     # Maybe plot points later
 
-    return xpos,ypos,x,y
+    return x,y
 
+def Motion():
+    xBelieveMotion = speed* math.cos(math.radians(angle))
+    yBelieveMotion = speed* math.sin(math.radians(angle))
+    
+    return xBelieveMotion, yBelieveMotion
 
 #Changes direction randomnly based on the angles it comes in and which wall it hits
 def changeDirection(xMotion,yMotion,directionAngle,wall):
@@ -110,39 +125,35 @@ def findNextDirection(xMotion,yMotion,directionAngle,d,wall):
 # After a wall hit, I move it back in the field as it sometimes likes to jump out
 def adjustPosition(directionAngle,wall,x,y):
     if directionAngle >= 180 and wallList[wall][1] == wallList[wall][3]:
-        y= y+speed
+        y = y+speed+1
     if directionAngle < 180 and wallList[wall][1] == wallList[wall][3]:
-        y = y -speed
+        y = y -speed-1
     if (directionAngle > 270 or (directionAngle > 0 and directionAngle < 90)) and wallList[wall][0] == wallList[wall][2]:
-        x = x-speed
+        x = x-speed-1
     if (directionAngle <= 270 and directionAngle > 90) and wallList[wall][0] == wallList[wall][2]:
-        x= x+speed
+        x= x+speed+1
     return x,y
 
 # If it gets stuck in a wall, which it sometimes does on corners, I move it a little in the right direction
 def stuck(directionAngle,wall,x,y):
     if directionAngle >= 180 and wallList[wall][0] == wallList[wall][2]:
-        y= y+radius
+        y= y+radius*0.5
     if directionAngle < 180 and wallList[wall][0] == wallList[wall][2]:
-        y = y -radius
+        y = y -radius*0.5
     if (directionAngle > 270 or (directionAngle > 0 and directionAngle < 90)) and wallList[wall][1] == wallList[wall][3]:
-        x = x-radius
+        x = x-radius*0.5
     if (directionAngle <= 270 and directionAngle > 90) and wallList[wall][1] == wallList[wall][3]:
-        x= x+radius
+        x= x+radius*0.5
     return x,y
 
 # Sensor Inputs, returns the endpoints of the sensor lines
-def sensors(centerX, centerY,angle):
+def sensors(centerX, centerY):
     sensorEndpoints = []
 
     for i in range(12):
         point1 = centerX, centerY
         distance = getSensorDistance(i,centerX,centerY)
-        #print(i , " " ,distance)
-        
-        point2 = centerX + distance * math.cos(i * 30*3.14/180+angle), centerY + distance * math.sin(i * 30 * 3.14/180+angle)
-        
-
+        point2 = centerX + distance * math.cos(i * 30*3.14/180+ math.radians(angle)), centerY + distance * math.sin(i * 30 * 3.14/180+math.radians(angle))
         pygame.draw.line(screen,  BLACK, point1, point2, 1)        
         sensorEndpoints.append([ centerX + distance * math.cos(i * 30*3.14/180+angle),  centerY + distance * math.sin(i * 30 * 3.14/180+angle)])
     return sensorEndpoints
@@ -161,30 +172,52 @@ def getSensorDistance(sensor,x,y):
         x2 = wallList[i][2]
         y2 = wallList[i][3]
 
- #       pygame.draw.line(screen,BLACK,(x,y),(x + 1000 * math.cos(sensor * 30*3.14/180+angle),y + 1000 * math.sin(sensor * 30 * 3.14/180+angle)))
  
         line1 = LineString([(x1,y1), (x2,y2)])
-        line2 = LineString([(x,y), (x + 1000 * math.cos(sensor * 30*3.14/180+angle), y + 1000 * math.sin(sensor * 30 * 3.14/180+angle))])
+        line2 = LineString([(x,y), (x + 1000 * math.cos(sensor * 30*3.14/180+math.radians(angle)), y + 1000 * math.sin(sensor * 30 * 3.14/180+math.radians(angle)))])
         point = line1.intersection(line2)
         point = numpy.array(point)
         
         if len(point) > 0:
             distance = ((point[0] - x)**2 + (point[1] - y)**2)**(1/2)
-            #print(distance, " " , sensor)
             if distance < closestWallDistance:
                 closestWallDistance = distance
                 closestWall = i
 
     # Introduce Noise
 
-    closestWallDistance = randint(80, 120)*closestWallDistance*(1/100);
+    closestWallDistance = randint(90, 110)*closestWallDistance*(1/100);
 
     return closestWallDistance
 
-        
+def SampleNormalDistribution(b) :
+    value =0
+    for i in range(0,12):
+        value = value + uniform(-b,b)
+    value = value/2
+    return value
+
+def sampleMotionModel(rot1,rot2,trans,x,y,angle):
+    #print(rot1)
+    rot1 = rot1 + SampleNormalDistribution(noise1*abs(rot1) + noise2*trans)
+    trans = trans + SampleNormalDistribution(noise3*trans + noise4*(abs(rot1)+abs(rot2)))
+    rot2 = rot2+ SampleNormalDistribution(noise1*abs(rot2) + noise2*trans)
+    x = x + trans*math.cos(math.radians((angle + rot1)%360))
+    y = y + trans*math.sin(math.radians((angle + rot1)%360))
+    angle = (angle + rot1 + rot2)%360
     
-angle = updateAngle(x,y,x+1,y)
-rotx, roty = calcDirection(x,y)
+
+    return x ,y,angle
+
+
+
+
+
+
+
+
+#angle = updateAngle(x,y,x+1,y)
+#rotx, roty = calcDirection(x,y)
 stuckInWall = 0;
 
 
@@ -219,35 +252,57 @@ while not done:
     for event in pygame.event.get():   # User did something
         if event.type == pygame.QUIT:  # If user clicked close
             done = True   # Flag that we are done so we exit this loop
-    oldx = x
-    oldy = y
+
+
+    if (believeY < 299 or believeY > 301) or (believeX < 129 or believeX >131):
+       rot1 = updateAngle(believeX, believeY, 130,300)
+       rot2 = 0
+       rot1 = (math.degrees(rot1) - angle)
+       xBelieveMotion, yBelieveMotion = Motion()
+       believeX, believeY = updatePosition(believeX,believeY,xBelieveMotion,yBelieveMotion)
+       x,y,angle = sampleMotionModel(rot1,rot2,speed,x,y,angle)
+       rotx,roty = calcDirection(x,y)
+       xpos = int(x)
+       ypos = int(y)
+       xpos2 = int(believeX)
+       ypos2 = int(believeY)
+       rotx2, roty2 = calcDirection(believeX,believeY)
+
+        
+       
+       #print(believeX, " ",x, " ", believeY, " ", y)
+       sensors(xpos,ypos)
+
+    else:
+        done = True
+
+ 
+        
     
-    xpos,ypos,x,y = updatePosition(x,y,xMotion,yMotion,angle)
-    angle = updateAngle(oldx,oldy,x,y)
-    rotx,roty= calcDirection(x,y) # this is for the pointer
+ #   xpos,ypos,x,y = updatePosition(x,y,xMotion,yMotion,angle)
+ #   angle = updateAngle(oldx,oldy,x,y)
+ #   rotx,roty= calcDirection(x,y) # this is for the pointer
 
+    circle = pygame.draw.circle(screen, GREEN , (xpos2,ypos2), radius,0)
+    circle = pygame.draw.circle(screen, BLACK , (xpos2,ypos2), radius,2)
+    line = pygame.draw.line(screen,BLACK, (xpos2,ypos2),(rotx2, roty2),2)
 
-
+    
     circle = pygame.draw.circle(screen, WHITE , (xpos,ypos), radius,0)
     circle = pygame.draw.circle(screen, BLACK , (xpos,ypos), radius,2)
     line = pygame.draw.line(screen,BLACK, (xpos,ypos),(rotx, roty),2)
 
+
     if (x > 1080 or x< 0 or y >720 or y < 0):
         x = 74
-        y=100
+        y= 100
     if stuckInWall > 0:
         stuckInWall = stuckInWall -1
     
     # Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(30)
  
 # Be IDLE friendly. If you forget this line, the program will 'hang'
 # on exit.
 pygame.quit()
-
-
-
-
-
-
