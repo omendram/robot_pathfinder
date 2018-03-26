87045104fc85ca765f8d700bf19e1b434d6fb558
YuriKazakov <jonas@schmidt-rhld.de>
2018-03-26 16:53:11 +0200

    Final Version

--- assignment2.py
+++ assignment2.py
@@ -4,27 +4,26 @@ import random, sys, time, math, numpy, shapely
 from numpy import linalg
 from random import randint, uniform
 from shapely.geometry import LineString
-from random import *
- 
+
 # Initialize the game engine
 pygame.init()
- 
+
 BLACK = [0, 0, 0]
 WHITE = [255, 255, 255]
 GREEN = [0, 255, 0]
-PINK = [255,200,220]
- 
+PINK = [247,98,106]
+
 # Set the height and width of the screen
 SIZE = [1080, 720]
- 
+
 screen = pygame.display.set_mode((1080,720),0,32)
 pygame.display.set_caption("Maze")
- 
- 
+
+
 clock = pygame.time.Clock()
 
 angle = 0
-speed = 2
+speed = 5
 
 
 x = 90
@@ -45,8 +44,8 @@ rot1=0
 rot2=0
 trans =0
 directionAngle =0
-noise1 = 0.4 # noise for rot1 and 2
-noise2 = 0.2 # trans noise for rot1 and 2
+noise1 = 0.2 # noise for rot1 and 2
+noise2 = 0.1 # trans noise for rot1 and 2
 noise3 = 0.4 # trans noise for trans
 noise4 = 0.2 # rot noise for trans
 
@@ -55,6 +54,8 @@ noise4 = 0.2 # rot noise for trans
 wallList = [[10,10,1070,10],[10,10,10,710],[10,710,1070,710],[1070,10,1070,710],[267,10,267,236],[534,236,534,472],[10,472,534,472],[801,10,801,472]]
 pointsToVisit = [[130, 350],[400,350],[400,100],[650,100],[650,600],[950,600],[950,100]]
 done = 0
+trailOfPositions = []
+trailSize = 20
 
 '''
 KALMAN STUFF
@@ -62,7 +63,7 @@ KALMAN STUFF
 import numpy as np
 
 
-X = np.matrix('90. 90. 1. 1.').T 
+X = np.matrix('90. 90. 1. 1.').T
 P = np.matrix(np.eye(4))*1000 # initial uncertainty
 R = 0.01**2
 
@@ -70,11 +71,11 @@ def kalman_xy(x, P, measurement, R,
               motion = np.matrix('0. 0. 0. 0.').T,
               Q = np.matrix(np.eye(4))):
     """
-    Parameters:    
+    Parameters:
     x: initial state 4-tuple of location and velocity: (x0, x1, x0_dot, x1_dot)
     P: initial uncertainty convariance matrix
     measurement: observed position
-    R: measurement noise 
+    R: measurement noise
     motion: external motion added to state vector x
     Q: motion noise (same shape as P)
     """
@@ -103,9 +104,9 @@ def kalman(x, P, measurement, R, motion, Q, F, H):
         Return: the updated and predicted new values for (x, P)
         See also http://en.wikipedia.org/wiki/Kalman_filter
         This version of kalman can be applied to many different situations by
-        appropriately defining F and H 
+        appropriately defining F and H
     '''
-    # UPDATE x, P based on measurement m    
+    # UPDATE x, P based on measurement m
     # distance between measured and current position-belief
     y = np.matrix(measurement).T - H * x
     S = H * P * H.T + R  # residual convariance
@@ -122,7 +123,7 @@ def kalman(x, P, measurement, R, motion, Q, F, H):
 
 '''
 def demo_kalman_xy():
-    x = np.matrix('0. 0. 0. 0.').T 
+    x = np.matrix('0. 0. 0. 0.').T
     P = np.matrix(np.eye(4))*1000 # initial uncertainty
     result = []
     R = 0.01**2
@@ -154,7 +155,7 @@ def makeWalls(wall):
 #Checks if there is a collision
 def CheckCollision(wall,xpos1,ypos1):
     x1, y1 = wall[0],wall[1]
-    x2, y2 = wall[2],wall[3] 
+    x2, y2 = wall[2],wall[3]
     x0, y0 = xpos1,ypos1
     nom = abs((y2 - y1) * x0 - (x2 - x1) * y0 + x2 * y1 - y2 * x1)
     denom = ((y2 - y1)**2 + (x2 - x1) ** 2) ** 0.5
@@ -178,7 +179,7 @@ def updatePosition(x,y,xMotion,yMotion):
 def Motion(angle):
     xBelieveMotion = speed* math.cos(math.radians(angle))
     yBelieveMotion = speed* math.sin(math.radians(angle))
-    
+
     return xBelieveMotion, yBelieveMotion
 
 #Changes direction randomnly based on the angles it comes in and which wall it hits
@@ -191,7 +192,7 @@ def changeDirection(xMotion,yMotion,directionAngle,wall):
         directionAngle2 = randint(110,250)
     if (directionAngle <= 270 and directionAngle > 90) and wallList[wall][0] == wallList[wall][2]:
         directionAngle2 = randint(-70,70)%360
-              
+
     xMotion =speed* math.cos(math.radians(directionAngle2))
     yMotion =speed* math.sin(math.radians(directionAngle2))
     return xMotion,yMotion, directionAngle2
@@ -199,10 +200,10 @@ def changeDirection(xMotion,yMotion,directionAngle,wall):
 
 # Finds the next direction of the robot
 def findNextDirection(xMotion,yMotion,directionAngle,d,wall):
-    
+
     xMotion,yMotion,directionAngle = changeDirection(xMotion,yMotion,directionAngle,wall)
     for i in range(0,8):
-        d = CheckCollision(wallList[i],x+xMotion,y+yMotion)           
+        d = CheckCollision(wallList[i],x+xMotion,y+yMotion)
     return xMotion,yMotion,directionAngle
 
 # After a wall hit, I move it back in the field as it sometimes likes to jump out
@@ -237,10 +238,10 @@ def sensors(centerX, centerY):
     for i in range(24):
         #point1 = centerX, centerY
         distance = getSensorDistance(i,centerX,centerY)
-        #point2 = centerX + distance[0] * math.cos(i * 15*3.14/180+ math.radians(angle)), centerY + distance[0] * math.sin(i * 15 * 3.14/180+math.radians(angle))
+        #point2 = centerX + distance[0] * math.cos(i * 15*math.pi/180+ math.radians(angle)), centerY + distance[0] * math.sin(i * 15 * math.pi/180+math.radians(angle))
         #pygame.draw.line(screen,  BLACK, point1, point2, 1)
-        sensorEpX = centerX + distance[0] * math.cos(i * 15*3.14/180+math.radians(angle))
-        sensorEpY = centerY + distance[0] * math.sin(i * 15 * 3.14/180+math.radians(angle))
+        sensorEpX = centerX + distance[0] * math.cos(i * 15*math.pi/180+math.radians(angle))
+        sensorEpY = centerY + distance[0] * math.sin(i * 15 * math.pi/180+math.radians(angle))
 
         sensorX = sensorX + centerXEstimatesFromCensor(i, sensorEpX, distance[1], math.radians(angle))
         sensorY = sensorY + centerYEstimatesFromCensor(i, sensorEpY, distance[1], math.radians(angle))
@@ -251,30 +252,30 @@ def sensors(centerX, centerY):
 # Return estimates of X and Y based on the sensor readings, including the noise
 # Use this along with odometry estimates to feed into KF and , determine the position of the robot
 def centerXEstimatesFromCensor(i, sensorEpX, distance, angle):
-    return sensorEpX - distance*math.cos(i * 15*3.14/180 + angle)
+    return sensorEpX - distance*math.cos(i * 15*math.pi/180 + angle)
 
 def centerYEstimatesFromCensor(i, sensorEpY, distance, angle):
-    return sensorEpY - distance*math.sin(i * 15*3.14/180 + angle)
+    return sensorEpY - distance*math.sin(i * 15*math.pi/180 + angle)
 
 def getSensorDistance(sensor,x,y):
     closestWall = 0
     closestWallDistance =1000
     distance = 0
-    
-    
+
+
     for i in range(0,8):
-        
+
         x1 = wallList[i][0]
         y1 = wallList[i][1]
         x2 = wallList[i][2]
         y2 = wallList[i][3]
 
- 
+
         line1 = LineString([(x1,y1), (x2,y2)])
-        line2 = LineString([(x,y), (x + 1000 * math.cos(sensor * 15*3.14/180+math.radians(angle)), y + 1000 * math.sin(sensor * 15 * 3.14/180+math.radians(angle)))])
+        line2 = LineString([(x,y), (x + 1000 * math.cos(sensor * 15*math.pi/180+math.radians(angle)), y + 1000 * math.sin(sensor * 15 * math.pi/180+math.radians(angle)))])
         point = line1.intersection(line2)
         point = numpy.array(point)
-        
+
         if len(point) > 0:
             distance = ((point[0] - x)**2 + (point[1] - y)**2)**(1/2)
             if distance < closestWallDistance:
@@ -282,10 +283,10 @@ def getSensorDistance(sensor,x,y):
                 closestWall = i
 
     # Introduce Noise
-    return [randint(85, 115)*closestWallDistance*(1/100), closestWallDistance]
+    return [randint(90, 110)*closestWallDistance/100, closestWallDistance]
 
 def SampleNormalDistribution(b) :
-    value =0
+    value = 0
     for i in range(0,12):
         value = value + uniform(-b,b)
     value = value/2
@@ -299,9 +300,9 @@ def sampleMotionModel(rot1,rot2,trans,x,y,angle):
     x = x + trans*math.cos(math.radians((angle + math.degrees(rot1))%360))
     y = y + trans*math.sin(math.radians((angle + math.degrees(rot1))%360))
     angle = (angle + math.degrees(rot1) + math.degrees(rot2))%360
-    
 
-    return x ,y,angle
+
+    return x,y,angle
 
 
 
@@ -331,16 +332,16 @@ while done < 7:
                 xMotion,yMotion,directionAngle = changeDirection(xMotion,yMotion,directionAngle,i)
                 updatePosition(x,y,xMotion,yMotion)
                 break
-            
+
             stuckInWall = 3
             x,y = adjustPosition(directionAngle,i,x,y)
             xMotion, yMotion,directionAngle = findNextDirection(xMotion,yMotion,directionAngle,d,i)
             break
 
 
-    
 
-    if (believeY < pointsToVisit[done][1]-3 or believeY > pointsToVisit[done][1]+3) or (believeX < pointsToVisit[done][0]-3 or believeX >pointsToVisit[done][0]+3):
+
+    if (believeY < pointsToVisit[done][1]-speed or believeY > pointsToVisit[done][1]+speed) or (believeX < pointsToVisit[done][0]-speed or believeX >pointsToVisit[done][0]+speed):
         rot1 = updateAngle(believeX, believeY, pointsToVisit[done][0],pointsToVisit[done][1])
         rot2 = 0
         rot1 = (math.degrees(rot1) - angle)
@@ -360,25 +361,33 @@ while done < 7:
 
         xpos2 = int(believeX)
         ypos2 = int(believeY)
+        trailOfPositions.append([xpos2,ypos2])
+        if(len(trailOfPositions)>trailSize):
+            del trailOfPositions[0]
         rotx2, roty2 = calcDirection(believeX,believeY, angle+rot1)
 
     else:
         done = done + 1
 
-    
-    
+
+    for pos in trailOfPositions:
+        pygame.draw.circle(screen, PINK, (pos[0],pos[1]), radius,0)
+        pygame.draw.circle(screen, BLACK , (pos[0],pos[1]), radius,2)
+
  #   xpos,ypos,x,y = updatePosition(x,y,xMotion,yMotion,angle)
  #   angle = updateAngle(oldx,oldy,x,y)
  #   rotx,roty= calcDirection(x,y) # this is for the pointer
 
-    circle = pygame.draw.circle(screen, GREEN , (xpos2,ypos2), radius,0)
-    circle = pygame.draw.circle(screen, BLACK , (xpos2,ypos2), radius,2)
+    pygame.draw.circle(screen, GREEN , (xpos2,ypos2), radius,0)
+    pygame.draw.circle(screen, BLACK , (xpos2,ypos2), radius,2)
     #line = pygame.draw.line(screen,BLACK, (xpos2,ypos2),(rotx2, roty2),2)
 
-    
-    circle = pygame.draw.circle(screen, WHITE , (xpos,ypos), radius,0)
-    circle = pygame.draw.circle(screen, BLACK , (xpos,ypos), radius,2)
- #   line = pygame.draw.line(screen,BLACK, (xpos,ypos),(rotx, roty),2)
+
+    pygame.draw.circle(screen, WHITE , (xpos,ypos), radius,0)
+    pygame.draw.circle(screen, BLACK , (xpos,ypos), radius,2)
+    pygame.draw.line(screen,BLACK, (xpos,ypos),(rotx, roty),2)
+
+
 
 
     if (x > 1080 or x< 0 or y >720 or y < 0):
@@ -393,7 +402,7 @@ while done < 7:
     # Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
     clock.tick(60)
- 
+
 # Be IDLE friendly. If you forget this line, the program will 'hang'
 # on exit.
 pygame.quit()
